950aada280266c6f8729daf3ded09708401d05bf
Sadman Sakib <46521025+SadmanSakib5597@users.noreply.github.com>
2020-10-20 07:33:01 +0600

    Add files via upload
    users information update

---
+++ User.dart
@@ -0,0 +1,11 @@
+class User{
+
+  static String uid;
+  static String userName;
+  static String userEmail;
+  static String password;
+  static String userPhoneNo;
+  static String userRegNo;
+
+
+}
